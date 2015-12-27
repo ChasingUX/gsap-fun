ab3d40941c915748486a70138291bfc083158836
Jesse Chase <jesse@digitalocean.com>
2015-12-27 16:04:40 -0500

    advanced logo build complete

--- source/javascripts/demoE.js
+++ source/javascripts/demoE.js
@@ -8,9 +8,11 @@ var $body = $('body'),
     $typePath = $('.type path'),
     $type = $('.type'),
     $rect = $('rect'),
+    $page = $('.page'),
     $rect1 = $('.rect1'),
     $rect2 = $('.rect2'),
     $rect3 = $('.rect3'),
+    $quotes = $('blockquote'),
     tlMaster;
 
 
@@ -25,14 +27,14 @@ var $body = $('body'),
     .fromTo($rect1, 1, {autoAlpha: 1, strokeDasharray: 217, strokeDashoffset: 217}, {autoAlpha: 1, strokeDasharray: 217, strokeDashoffset: 0, ease: Power1.easeInOut}, 'arcIn-=2.2')
     .fromTo($rect2, 1, {autoAlpha: 1, strokeDasharray: 182, strokeDashoffset: 182}, {autoAlpha: 1, strokeDasharray: 182, strokeDashoffset: 0, ease: Power1.easeInOut}, 'arcIn-=2')
     .fromTo($rect3, 1, {autoAlpha: 1, strokeDasharray: 140, strokeDashoffset: 140}, {autoAlpha: 1, strokeDasharray: 140, strokeDashoffset: 0, ease: Power1.easeInOut}, 'arcIn-=1.8')
-  
-    .to([$rect,$logoArc], .3, {fill: '#2B63FF', ease: Power2.easeOut}, 'arcIn-=.8')
+    .to([$rect,$logoArc], .3, {fill: '#ffffff', ease: Power2.easeOut}, 'arcIn-=.8')
     .to($mark, 1.1, {x: 0, ease: Power3.easeInOut}, 'arcIn-=1.4')
-    //.fromTo($typePath, 1, {autoAlpha: 0}, {autoAlpha: 1, ease: Power1.easeInOut}, 'arcIn-=2')
+    .fromTo($typePath, .8, {autoAlpha: 0, fill: "#ffffff", x: -20}, {autoAlpha: 1, x: 0, ease: Power1.easeOut}, 'arcIn-=.8')
+    .to($body, .5, {backgroundColor: "#2B63FF", ease: Power0.easeNone},'-=1.1')
+    .to($logo, 1.6, {scale: .6, top: '30px', ease: CubicBezier.config(0.51, -0.47, 0.21, 1)})
+    .fromTo($page, 1, {autoAlpha: 0, y: 40}, {autoAlpha: 1, y: 0},'-=.7')
+    .staggerFromTo($quotes, 3, {autoAlpha: 0, y: '+=25'}, {autoAlpha: 1, y: '0'}, 0.5, '-=.1')
 
-    .fromTo($typePath, .8, {autoAlpha: 0, fill: "#2B63FF", x: -20}, {autoAlpha: 1, x: 0, ease: Power1.easeOut}, 'arcIn-=.8')
-    .to($logo, 1.4, {scale: .7, y: -300, ease: CubicBezier.config(0.53, -0.75, 0.28, 1.01)})
-    
   
 });
 
