fc8adb6cd6d7405b2dd3b1644f3943c45da260f9
Jesse Chase <jesse@digitalocean.com>
2015-12-27 17:38:44 -0500

    yep another

--- source/javascripts/demoE.js
+++ source/javascripts/demoE.js
@@ -47,8 +47,7 @@ var $body = $('body'),
 
   function repeat() {
     var $logo = $('.logo'),
-    $quotes = $('blockquote'),
-    $quotesReverse = $quotes.get().reverse();
+    $quotes = $('blockquote');
 
     if($('body').hasClass('logoLoaded')) {
       $body.removeClass('logoLoaded');
@@ -56,7 +55,8 @@ var $body = $('body'),
       tlBack
         .to($logo, 1.4, {top: '-=160', ease: CubicBezier.config(0.7, -0.42, 0.55, 1)})
         .to($body, .5, {backgroundColor: "#ffffff", ease: Power0.easeNone},'-=.9')
-        .staggerFromTo($quotesReverse, 1, {autoAlpha: 1, y: '0'}, {autoAlpha: 0, y: '+=25'}, 0.1, '-=.9')
+        .to($quotes, .5, {autoAlpha: 0, ease: Power0.easeNone},'-=.9')
+        //.staggerFromTo($quotesReverse, 1, {autoAlpha: 1, y: '0'}, {autoAlpha: 0, y: '+=25'}, 0.1, '-=.9')
         .fromTo($page, 1.2, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 140},'-=.9')
 
         .set($quotes, {y: 0})
