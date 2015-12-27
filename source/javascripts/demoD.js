fdfacbf7bc4d08444a7e39a19ad622cd1880e9ff
Jesse Chase <jesse@digitalocean.com>
2015-12-27 00:22:01 -0500

    sliding menu for slideshow

--- source/javascripts/demoD.js
+++ source/javascripts/demoD.js
@@ -1,1 +1,22 @@
+$(function() {
+  var $normalNav = $('nav').not('.slideNav'),
+    $hamburger = $('li.home'),
+    $hamburgerBuns = $hamburger.find('span');
 
+  $("nav").hover(function(event){}, function(){
+    var name = $(event.toElement).attr('class');
+    if (name == 'bg'){
+      if($(this).attr('class') != 'slideNav'){
+        TweenLite.to($normalNav, .5, {bottom: '-50px'});
+        TweenLite.to($hamburger, .5, {top: '-50px'});
+        TweenLite.to($hamburgerBuns, .5, {backgroundColor: '#ffffff'});
+      }
+    } else {
+      if($(this).attr('class') == 'slideNav') {
+        TweenLite.to($normalNav, .5, {bottom: '0'});
+        TweenLite.to($hamburger, .5, {top: '0'});
+        TweenLite.to($hamburgerBuns, .5, {backgroundColor: '#2B63FF'});
+      } 
+    }
+  });
+});
